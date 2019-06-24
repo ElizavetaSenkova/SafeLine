42ba2d60e07017374bdb53933e45eb574ba80549
Liza <lizok9910@mail.ru>
2019-06-24 17:44:32 +0300

    Добавляет шапку и начинает основной контент

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,51 @@
 $(function() {
 
-	// Custom JS
+	// parameters-slider
+
+	$("#parameters__slider").slick({
+    dots: true,
+    customPaging: function(slider, i) { 
+        return '<button class="tab">' + $(slider.$slides[i]).find('[data-title]').data('title') + '<i class="fa fa-sort-asc"></i></button>';
+		},
+		appendDots:'.custom-dots',
+		slidesToShow: 1,
+		arrows: false,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1500,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+		]
+});
+
+	// parameters-slider
+
+	// assortment-slider
+
+	$("#assortment__slider").slick({
+    dots: true,
+    customPaging: function(slider, i) { 
+        return '<button class="tab">' + $(slider.$slides[i]).find('[data-title]').data('title') + '<i class="fa fa-sort-asc"></i></button>';
+		},
+		appendDots:'.custom-dots2',
+		slidesToShow: 1,
+		arrows: false,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1500,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+		]
+});
+
+	// assortment-slider
 
 });
